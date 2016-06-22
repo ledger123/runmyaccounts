38379145c80c44f6d175dd93eae1b9a1f9db7e40
nils <nsamuelsson@runmyaccounts.com>
2016-06-22 15:55:44 +0200

    Script runs always

--- sql/Pg-upgrade-2.8.10-2.8.11.sql
+++ sql/Pg-upgrade-2.8.10-2.8.11.sql
@@ -1,12 +1,17 @@
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM information_schema.tables where table_name = 'dispatch')
+  THEN
+    -- dispatch methods table
+    CREATE TABLE dispatch (
+      id int DEFAULT nextval('id'),
+      description text
+    );
 
--- dispatch methods table
-CREATE TABLE IF NOT EXISTS dispatch (
-  id int DEFAULT nextval('id'),
-  description text
-);
-
-ALTER TABLE customer ADD dispatch_id INTEGER;
-ALTER TABLE vendor ADD dispatch_id INTEGER;
-
---
+    ALTER TABLE customer ADD dispatch_id INTEGER;
+    ALTER TABLE vendor ADD dispatch_id INTEGER;
+  END IF;
+END
+$$
+;
 update defaults set fldvalue = '2.8.11' where fldname = 'version';
