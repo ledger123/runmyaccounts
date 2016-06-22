d4db7b046a6b073840561ce7ee9fcf879ee0a256
nils <nsamuelsson@runmyaccounts.com>
2016-06-22 16:18:39 +0200

    Revert "Script runs always"
    This reverts commit 38379145c80c44f6d175dd93eae1b9a1f9db7e40.

--- sql/Pg-upgrade-2.8.10-2.8.11.sql
+++ sql/Pg-upgrade-2.8.10-2.8.11.sql
@@ -1,17 +1,12 @@
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM information_schema.tables where table_name = 'dispatch')
-  THEN
-    -- dispatch methods table
-    CREATE TABLE dispatch (
-      id int DEFAULT nextval('id'),
-      description text
-    );
 
-    ALTER TABLE customer ADD dispatch_id INTEGER;
-    ALTER TABLE vendor ADD dispatch_id INTEGER;
-  END IF;
-END
-$$
-;
+-- dispatch methods table
+CREATE TABLE IF NOT EXISTS dispatch (
+  id int DEFAULT nextval('id'),
+  description text
+);
+
+ALTER TABLE customer ADD dispatch_id INTEGER;
+ALTER TABLE vendor ADD dispatch_id INTEGER;
+
+--
 update defaults set fldvalue = '2.8.11' where fldname = 'version';
