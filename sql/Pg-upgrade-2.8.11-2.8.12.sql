1f825b1d9d8307228728073e1e81bff66f682757
Armaghan Saqib <ar.saqib@gmail.com>
2017-04-07 10:41:15 +0500

    #68 more updates to upgrade script

--- sql/Pg-upgrade-2.8.11-2.8.12.sql
+++ sql/Pg-upgrade-2.8.11-2.8.12.sql
@@ -3,12 +3,15 @@ ALTER TABLE acc_trans DROP COLUMN tax_chart_id;
 ALTER TABLE ar DROP COLUMN linetax;
 ALTER TABLE ap DROP COLUMN linetax;
 
+----
 
 ALTER TABLE invoicetax ADD amount float;
 
+-- AP
+
 UPDATE invoicetax it
 SET amount = (
-    SELECT i.qty * i.sellprice * -1 
+    SELECT i.qty * i.fxsellprice * -1 
     FROM invoice i 
     WHERE i.id = it.invoice_id
 )
@@ -18,30 +21,35 @@ UPDATE invoicetax
 SET amount = amount * (SELECT exchangerate FROM ap WHERE ap.id = invoicetax.trans_id)
 WHERE trans_id IN (SELECT id FROM ap);
 
+-- AR
+
 UPDATE invoicetax it
 SET amount = (
-    SELECT i.qty * i.sellprice
+    SELECT i.qty * i.fxsellprice
     FROM invoice i 
     WHERE i.id = it.invoice_id
 )
 WHERE trans_id IN (SELECT id FROM ar);
 
 UPDATE invoicetax
-SET amount = amount * (SELECT exchangerate FROM ap WHERE ap.id = invoicetax.trans_id)
+SET amount = amount * (SELECT exchangerate FROM ar WHERE ar.id = invoicetax.trans_id)
 WHERE trans_id IN (SELECT id FROM ar);
 
+--
 
 ALTER TABLE acc_trans ADD tax_chart_id INTEGER;
 UPDATE acc_trans 
 SET tax_chart_id = (select id from chart where accno = substr(tax,1,4)) 
 WHERE tax <> '' and tax <> 'auto';
 
+--
 
 ALTER TABLE ar ADD linetax BOOLEAN DEFAULT false;
 UPDATE ar SET linetax = '1' WHERE NOT invoice AND id IN (
     SELECT DISTINCT trans_id FROM acc_trans WHERE tax <> ''
 );
 
+--
 
 ALTER TABLE ap ADD linetax BOOLEAN DEFAULT false;
 UPDATE ap SET linetax = '1' WHERE NOT invoice AND id IN (
@@ -50,5 +58,4 @@ UPDATE ap SET linetax = '1' WHERE NOT invoice AND id IN (
 
 --
 -- update defaults set fldvalue = '2.8.12' where fldname = 'version';
-
 
