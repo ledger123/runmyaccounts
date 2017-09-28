d66a82e2d9ff450a48e7142bc53ff0036137920d
nils <nsamuelsson@runmyaccounts.com>
2017-09-28 15:55:14 +0200

    Moved dbupdate.sql to schema update 2.8.13

--- sql/Pg-custom_tables.sql
+++ sql/Pg-custom_tables.sql
@@ -147,3 +147,14 @@ ALTER TABLE acc_trans ADD tax_chart_id INTEGER;
 ALTER TABLE ar ADD linetax BOOLEAN DEFAULT false;
 ALTER TABLE ap ADD linetax BOOLEAN DEFAULT false;
 
+ALTER TABLE ar ADD COLUMN ts timestamp DEFAULT NOW();
+ALTER TABLE ap ADD COLUMN ts timestamp DEFAULT NOW();
+ALTER TABLE gl ADD COLUMN ts timestamp DEFAULT NOW();
+
+CREATE TABLE ar_log AS SELECT * FROM ar WHERE 1 = 2;
+CREATE TABLE ap_log AS SELECT * FROM ap WHERE 1 = 2;
+CREATE TABLE gl_log AS SELECT * FROM gl WHERE 1 = 2;
+CREATE TABLE acc_trans_log AS SELECT * FROM acc_trans WHERE 1 = 2;
+ALTER TABLE acc_trans_log ADD COLUMN ts timestamp DEFAULT NOW();
+
+update defaults set fldvalue = '2.8.13' where fldname = 'version';
