88eed2d10b54a3405606c3ee45546b2082f5d643
Armaghan Saqib <ar.saqib@gmail.com>
2017-10-12 14:22:33 +0500

    Fixed incorrect columns list in union in GL report

--- SL/GL.pm
+++ SL/GL.pm
@@ -702,7 +702,7 @@ sub transactions {
 		 $gdescription AS lineitem, '' AS name, '' AS vcnumber,
 		 '' AS address1, '' AS address2, '' AS city,
 		 '' AS zipcode, '' AS country, c.description AS accdescription,
-		 '' AS intnotes, '*' log, ac.ts
+		 '' AS intnotes, g.curr, g.exchangerate, '' log, g.ts
                  FROM gl g
 		 JOIN acc_trans_log ac ON (g.id = ac.trans_id)
 		 JOIN chart c ON (ac.chart_id = c.id)
@@ -718,7 +718,7 @@ sub transactions {
 		 $lineitem AS lineitem, ct.name, ct.customernumber,
 		 ad.address1, ad.address2, ad.city,
 		 ad.zipcode, ad.country, c.description AS accdescription,
-		 a.intnotes, '*' log, ac.ts
+		 a.intnotes, a.curr, a.exchangerate, '' log, a.ts
 		 FROM ar a
 		 JOIN acc_trans_log ac ON (a.id = ac.trans_id)
 		 $invoicejoin
@@ -737,7 +737,7 @@ sub transactions {
 		 $lineitem AS lineitem, ct.name, ct.vendornumber,
 		 ad.address1, ad.address2, ad.city,
 		 ad.zipcode, ad.country, c.description AS accdescription,
-		 a.intnotes, '*' log, ac.ts
+		 a.intnotes, a.curr, a.exchangerate, '' log, a.ts
 		 FROM ap a
 		 JOIN acc_trans_log ac ON (a.id = ac.trans_id)
 		 $invoicejoin
