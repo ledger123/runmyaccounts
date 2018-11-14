d74434e3ec3ff73fa56c0dfa7037497fee1513fd
Armaghan Saqib <ar.saqib@gmail.com>
2018-11-14 13:44:37 +0500

    #123 sorting on due column of ar/ap trans done

--- bin/mozilla/aa.pl
+++ bin/mozilla/aa.pl
@@ -2151,14 +2151,18 @@ sub transactions {
         $option   .= $locale->text('Paid Early');
     }
 
-    @columns = qw(transdate id invnumber ordnumber ponumber description name customernumber vendornumber address netamount tax amount paid paymentmethod due curr datepaid duedate memo notes intnotes till employee manager warehouse shippingpoint shipvia waybill dcn paymentdiff department);
+    @columns = qw(transdate id invnumber ordnumber ponumber description name customernumber vendornumber address netamount tax amount paid due paymentmethod due curr datepaid duedate memo notes intnotes till employee manager warehouse shippingpoint shipvia waybill dcn paymentdiff department);
 
     @columns = $form->sort_columns(@columns);
 
-    # Don't change column positions if it 'paid';
+    # Don't change column positions if it 'paid' or 'due';
     if ($form->{sort} eq 'paid'){
        shift @columns;
        push @columns, 'paid';
+    }
+    if ($form->{sort} eq 'due'){
+       shift @columns;
+       push @columns, 'due';
     }
 
     pop @columns if $form->{department};
@@ -2219,7 +2223,7 @@ sub transactions {
     $column_data{paid}          = "<th align=right><a class=listheading href=$href&sort=paid>" . $locale->text('Paid') . "</a></th>";
     $column_data{paymentmethod} = "<th><a class=listheading href=$href&sort=paymentmethod>" . $locale->text('Payment Method') . "</a></th>";
     $column_data{datepaid}      = "<th><a class=listheading href=$href&sort=datepaid>" . $locale->text('Date Paid') . "</a></th>";
-    $column_data{due}           = "<th class=listheading>" . $locale->text('Due') . "</th>";
+    $column_data{due}           = "<th align=right><a class=listheading href=$href&sort=due>" . $locale->text('Due') . "</a></th>";
     $column_data{notes}         = "<th class=listheading>" . $locale->text('Notes') . "</th>";
     $column_data{intnotes}      = "<th class=listheading>" . $locale->text('Internal Notes') . "</th>";
     $column_data{employee}      = "<th><a class=listheading href=$href&sort=employee>$employee</a></th>";
