e4692ada7b89b1a07f9ef50b76adb0d205f85a65
Armaghan Saqib <ar.saqib@gmail.com>
2019-06-03 15:00:39 +0500

    #142 added trans_id to datev processing

--- sql/Pg-custom_tables.sql
+++ sql/Pg-custom_tables.sql
@@ -148,7 +148,7 @@ ALTER TABLE ar ADD linetax BOOLEAN DEFAULT false;
 ALTER TABLE ap ADD linetax BOOLEAN DEFAULT false;
 
 -- DATEV export 
-create table debits (id serial, reference text, description text, transdate date, accno text, amount numeric(12,2));
-create table credits (id serial, reference text, description text, transdate date, accno text, amount numeric(12,2));
-create table debitscredits (id serial, reference text, description text, transdate date, debit_accno text, credit_accno text, amount numeric(12,2));
+create table debits (id serial, trans_id integer, reference text, description text, transdate date, accno text, amount numeric(12,2));
+create table credits (id serial, trans_id integer, reference text, description text, transdate date, accno text, amount numeric(12,2));
+create table debitscredits (id serial, trans_id integer, reference text, description text, transdate date, debit_accno text, credit_accno text, amount numeric(12,2));
 
