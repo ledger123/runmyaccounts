b96d599cab754d01b625b6c7f02f653a32bf1b1f
Armaghan Saqib <ar.saqib@gmail.com>
2019-08-26 08:48:56 +0500

    chart.js inclusion with sample code

--- bin/mozilla/reports.pl
+++ bin/mozilla/reports.pl
@@ -17,6 +17,62 @@ sub continue { &{$form->{nextsub}} };
 #
 #=================================================
 
+sub incomestatementchart {
+    use DBIx::Simple;
+    $form->{dbh} = $form->dbconnect(\%myconfig);
+    $form->{dbs} = DBIx::Simple->connect($form->{dbh});
+
+    $form->{title} = $locale->text('All Taxes Report');
+    &print_title;
+    print q|
+<script type="text/javascript" src=https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.js></script>
+|;
+
+   print q|
+<canvas id="myChart" width="400" height="400"></canvas>
+<script>
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+</script>
+|;
+
+}
+
 #-------------------------------
 sub alltaxes {
 
