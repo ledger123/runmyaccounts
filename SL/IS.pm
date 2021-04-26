6f71508ac842b246221b0ef1b619cd5d7bb4229a
Armaghan Saqib <ar.saqib@gmail.com>
2021-04-26 09:27:55 +0000

    #245 added discountrate_percent variable in sales invoices

--- SL/IS.pm
+++ SL/IS.pm
@@ -289,6 +289,7 @@ sub invoice_details {
       
       push(@{ $form->{discount} }, $discount);
       push(@{ $form->{discountrate} }, $form->format_amount($myconfig, $form->{"discount_$i"}));
+      push(@{ $form->{discountrate_percent} }, $form->format_amount($myconfig, $form->{"discount_$i"}) . '%');
 
       $form->{total} += $linetotal;
 
