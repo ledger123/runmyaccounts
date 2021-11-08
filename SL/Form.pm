8ef0db5b034a2671f1c6e410c47e88166550fef3
Armaghan Saqib <ar.saqib@gmail.com>
2021-11-08 05:59:36 +0000

    #169 disabled auto-complete to fix some issues first

--- SL/Form.pm
+++ SL/Form.pm
@@ -409,7 +409,7 @@ qq|<meta http-equiv="Content-Type" content="text/plain; charset=$self->{charset}
 		print q|
 <script>
 $(document).ready(function() {
-    $('.js-basic-single').select2();
+    $('.js-basic-single-disabled').select2();
 });
 $(document).on('select2:open', () => {
     document.querySelector('.select2-search__field').focus();
