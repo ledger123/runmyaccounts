3d82d257c6f893e51f55e6ae6bfeeec037da8eaa
phkroll <pkroll@runymyaccounts.com>
2022-02-07 15:01:03 +0100

    Add chat table for RMA UMUT Feature to DB Update version 2.8.21

--- sql/Pg-upgrade-2.8.20-2.8.21.sql
+++ sql/Pg-upgrade-2.8.20-2.8.21.sql
@@ -2,7 +2,16 @@ CREATE TABLE lastused (
     id      serial primary key,
     report  varchar(40),
     cols    text,
-    login   varchar(40)
+    login   varchar(255)
 );
+
+CREATE TABLE chat (
+	id SERIAL PRIMARY KEY,
+	trans_id INT NOT NULL,
+	message VARCHAR(255) NOT NULL,
+	employee_id INT NOT NULL,
+	creation_date TIMESTAMP NOT NULL
+);
+
 UPDATE defaults SET fldvalue = '2.8.21' where fldname = 'version';
 
