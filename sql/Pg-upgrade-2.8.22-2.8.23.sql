77869419c7c8e848da6690e2e4e3e97568908b6e
vreinok <vreinok@runmyaccounts.com>
2022-09-23 16:41:27 +0200

    115415 fix a script

--- sql/Pg-upgrade-2.8.22-2.8.23.sql
+++ sql/Pg-upgrade-2.8.22-2.8.23.sql
@@ -1,8 +1,8 @@
 -- Required for structured addresses modification
 
 ALTER TABLE address
-    ADD COLUMN post_office varchar(64),
-    ADD COLUMN is_migrated boolean
+    ADD COLUMN IF NOT EXISTS post_office varchar(64),
+    ADD COLUMN IF NOT EXISTS is_migrated boolean
 ;
 
 UPDATE defaults SET fldvalue = '2.8.23' where fldname = 'version';
