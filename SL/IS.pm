7331a443d7c62c653e60fe5c9f640b568f10ee41
stoldorma <stoldo@runmyaccounts.com>
2023-03-21 10:19:15 +0100

    revert unnseacry chnages

--- SL/IS.pm
+++ SL/IS.pm
@@ -762,6 +762,7 @@ sub invoice_details {
   # split strdbkginfqr into 2 lines, since doing this in latex causes display issues for special characters such as "_" (See #112444)
   $form->{strdbkginfline1qr} = substr($form->{strdbkginfqr}, 0, 50);
   $form->{strdbkginfline2qr} = substr($form->{strdbkginfqr}, 50, 85);
+  
 }
 
 
