728a514c256d67afb907fe99acabd6bc7bf16431
raphaelroos <raphael.roos@infoniqa.com>
2024-06-27 11:44:14 +0200

    add financial year table and link to yearend

--- sql/Pg-upgrade-2.8.32-2.8.33.sql
+++ sql/Pg-upgrade-2.8.32-2.8.33.sql
@@ -1,10 +1,17 @@
+ALTER TABLE yearend ADD id SERIAL;
+ALTER TABLE yearend ADD PRIMARY KEY (id);
+
 CREATE TYPE financial_year_status_enum AS ENUM ('OPEN', 'CURRENT_FINANCIAL_YEAR', 'CLOSED');
 
 CREATE TABLE IF NOT EXISTS financial_year (
 id SERIAL PRIMARY KEY,
 start_date DATE,
 end_date DATE,
-status financial_year_status_enum
+status financial_year_status_enum,
+yearend_id INT,
+    CONSTRAINT fk_yearend 
+    FOREIGN KEY (yearend_id) 
+    REFERENCES yearend(id)    
 );
 
 UPDATE defaults SET fldvalue = '2.8.33' where fldname = 'version';
