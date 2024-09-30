475b48a795018a78b200f1f7c60b02b8c6ecbec3
Armaghan Saqib <ar.saqib@gmail.com>
2024-09-30 06:25:26 +0500

    #561 changes to adjust date format to dd/mm/yyyy from iso when required

--- SL/Form.pm
+++ SL/Form.pm
@@ -4729,6 +4729,9 @@ sub isvaldate {
 		if ( $date ne $cleandate ) {
 			$self->error($text);
 		}
+        if ($date =~ /^\d{4}-\d{2}-\d{2}$/) {
+            $date =~ s/(\d{4})-(\d{2})-(\d{2})/$3-$2-$1/;
+        }
         if (!$self->{id}){
             my $current_date = $self->current_date($myconfig);
             my $datediff = $self->datediff($myconfig, $current_date, $date);
