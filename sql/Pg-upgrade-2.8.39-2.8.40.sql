667a9da1a8759048dea752f00937d2435d12fe4a
Bohte <bohdan.teslia@infoniqa.com>
2024-10-09 11:05:01 +0200

    IBP-1256 - conflict resolved

--- sql/Pg-upgrade-2.8.39-2.8.40.sql
+++ sql/Pg-upgrade-2.8.39-2.8.40.sql
@@ -1,4 +1,4 @@
 ALTER TABLE imported_transaction_to_booking
     ADD COLUMN booking_part_id INT;
 
-UPDATE defaults SET fldvalue = '2.8.39' WHERE fldname = 'version';
+UPDATE defaults SET fldvalue = '2.8.40' WHERE fldname = 'version';
