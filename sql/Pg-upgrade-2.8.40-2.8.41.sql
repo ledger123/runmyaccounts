1e0e297a0d052cb36d09708104332cc413f429e6
Bohte <bohdan.teslia@infoniqa.com>
2024-10-14 15:40:01 +0200

    IBP-1312 - text search engine configuration for related party matching

---
+++ sql/Pg-upgrade-2.8.40-2.8.41.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION fuzzystrmatch;
+
+CREATE TEXT SEARCH DICTIONARY simple_related_party(
+    TEMPLATE = simple,
+    STOPWORDS = related_party
+);
+
+CREATE TEXT SEARCH CONFIGURATION simple_related_party(
+    COPY = simple
+);
+ALTER TEXT SEARCH CONFIGURATION simple_related_party
+    ALTER MAPPING REPLACE simple WITH simple_related_party;
+
+UPDATE defaults SET fldvalue = '2.8.41' WHERE fldname = 'version';
