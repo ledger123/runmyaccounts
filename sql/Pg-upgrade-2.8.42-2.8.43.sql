484b0d7e5bae5668cc3c8a05c25fc0c8213aff17
Bohte <bohdan.teslia@infoniqa.com>
2025-01-10 15:28:44 +0100

    IBP-1420 - permission status removed from bank_account table

--- sql/Pg-upgrade-2.8.42-2.8.43.sql
+++ sql/Pg-upgrade-2.8.42-2.8.43.sql
@@ -1,5 +1,4 @@
 ALTER TABLE bank_account
-    ADD COLUMN permission_id INT,
-    ADD COLUMN permission_status TEXT;
+    ADD COLUMN blink_permission_id INT;
 
 UPDATE defaults SET fldvalue = '2.8.43' WHERE fldname = 'version';
