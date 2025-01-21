630e186021c24ed037866e7a65b662193bd0c8d5
Bohte <bohdan.teslia@infoniqa.com>
2025-01-21 16:21:59 +0100

    IBP-1420 - blink_account_id

--- sql/Pg-upgrade-2.8.42-2.8.43.sql
+++ sql/Pg-upgrade-2.8.42-2.8.43.sql
@@ -1,4 +1,5 @@
 ALTER TABLE bank_account
-    ADD COLUMN blink_permission_id BIGINT;
+    ADD COLUMN blink_permission_id BIGINT,
+    ADD COLUMN blink_account_id TEXT;
 
 UPDATE defaults SET fldvalue = '2.8.43' WHERE fldname = 'version';
