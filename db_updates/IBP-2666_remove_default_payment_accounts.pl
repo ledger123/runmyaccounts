b2642606bd48c34667c30565a59b6422b8e6be19
Bartosz Worobiec <Bartosz.Worobiec@infoniqa.com>
2026-01-12 15:38:15 +0100

    IBP-2666 remove_default_payment_accounts

--- db_updates/IBP-2666_remove_default_payment_accounts.pl
+++ db_updates/IBP-2666_remove_default_payment_accounts.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 use DBI;
+use File::Path qw(make_path);
+use POSIX qw(strftime);
 
 # Integration
 my $dbHost   = "192.168.8.17";
@@ -16,6 +18,29 @@ my $password = "";
 
 my $dbh = DBI->connect( $dsn, $userid, $password, { RaiseError => 1 } )
   or die $DBI::errstr;
+
+my $backup_dir = '/home/change_me';
+
+sub backup_customer_vendor {
+    my ($db, $dbh) = @_;
+    make_path($backup_dir) unless -d $backup_dir;
+    my $backup_file = "$backup_dir/${db}_customer_vendor_backup";
+    open my $fh, '>', $backup_file or do {
+        warn "[ERROR] Cannot open $backup_file for writing: $!\n";
+        return;
+    };
+    print $fh "# id,payment_accno_id\n";
+    for my $table (qw(customer vendor)) {
+        my $sth = $dbh->prepare("SELECT id, payment_accno_id FROM $table WHERE payment_accno_id IS NOT NULL AND payment_accno_id <> ''");
+        $sth->execute();
+        while (my $row = $sth->fetchrow_hashref) {
+            print $fh "$table,$row->{id},$row->{payment_accno_id}\n";
+        }
+        $sth->finish();
+    }
+    close $fh;
+    print "[OK] Backup for $db written to $backup_file\n";
+}
 
 sub update_db {
     my @queries = @_;
@@ -37,6 +62,8 @@ sub update_db {
             warn "[ERROR] Failed to connect to database '$db'. Error: $DBI::errstr\n";
             next;
         }
+
+        backup_customer_vendor($db, $dbh);
 
         for my $query (@queries) {
             my $rv = $dbh->do($query);
