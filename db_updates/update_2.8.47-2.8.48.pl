1b41177f19717b1c89d1f9b2efb77e2735557a72
Bartosz Worobiec <Bartosz.Worobiec@infoniqa.com>
2026-02-17 09:20:56 +0100

    DB Version raise and prepare sql updates

--- db_updates/update_2.8.47-2.8.48.pl
+++ db_updates/update_2.8.47-2.8.48.pl
@@ -65,6 +65,10 @@ ADD payment_clearing_accno_id integer
 ALTER TABLE vendor
 ADD payment_clearing_accno_id integer
 },
+
+    q{
+UPDATE defaults SET fldvalue = '2.8.48' WHERE fldname = 'version';
+},
 );
 
 $dbh->disconnect();
