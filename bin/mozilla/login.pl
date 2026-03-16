30a94ca7c3dc592c8ee6ee3728d69b334a8c37ac
Bartosz Worobiec <118178530+barworobiec@users.noreply.github.com>
2026-03-16 10:44:05 +0100

    Revert "Revert "dev to int""

--- bin/mozilla/login.pl
+++ bin/mozilla/login.pl
@@ -230,26 +230,23 @@ sub login {
 
 
   if (! $form->{beenthere}) {
-    open(FH, "$memberfile") or $form->error("$memberfile : $!");
-    @a = <FH>;
-    close(FH);
-    
-    foreach $item (@a) {
+    my $dbfile = "${memberfile}.db";
+    my $mdbh = DBI->connect("dbi:SQLite:dbname=$dbfile", "", "", {
+      RaiseError => 0,
+      PrintError => 0,
+      AutoCommit => 1,
+      sqlite_unicode => 1,
+    }) or $form->error("$dbfile : $DBI::errstr");
 
-      if ($item =~ /^\[(.*?)\]/) {
-	$login = $1;
-	$found = 1;
-      }
-
-      if ($item =~ /^company=/) {
-	if ($login =~ /^$form->{login}(@|$)/ && $found) {
-	  ($null, $name) = split /=/, $item, 2;
-	  $login{$login} = $name;
-	  $found = 0;
-	}
-
+    my $sth = $mdbh->prepare(qq|SELECT login, company FROM members WHERE login LIKE ?|);
+    $sth->execute("$form->{login}\%");
+    while (my ($l, $c) = $sth->fetchrow_array) {
+      if ($l =~ /^$form->{login}(@|$)/) {
+        $login{$l} = $c;
       }
     }
+    $sth->finish;
+    $mdbh->disconnect;
 
     if (keys %login > 1) {
       &selectdataset(\%login);
@@ -343,5 +340,4 @@ sub logout {
   $form->redirect;
 
 }
-
 
