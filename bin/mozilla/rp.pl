b80aec083c6b972b573d08ad04e9382dd35fe29d
bboehm <benjamin.boehm@infoniqa.com>
2026-04-23 12:43:23 +0200

    IBP-2423 Fix issue with wrong qr invoice data for reminders

--- bin/mozilla/rp.pl
+++ bin/mozilla/rp.pl
@@ -3398,7 +3398,8 @@ sub do_print_reminder {
 	);
 
 	@a =
-	  qw(id invnumber name nameqr address1 address1qr address2 city cityqr state stateqr zipcode zipcodeqr country contact typeofcontact salutation firstname lastname dcn iban rvc membernumber qriban strdbkginf invdescriptionqr dcn);
+	  qw(id invnumber name nameqr address1 address1qr address2 city cityqr state stateqr zipcode zipcodeqr country contact typeofcontact salutation firstname lastname dcn iban rvc membernumber qriban strdbkginf invdescriptionqr dcn
+	  qribanqr companyqr companyaddress1qr companyzipqr companycityqr invnumberqr invdateqr businessnumberqr strdbkginfqr strdbkginfline1qr strdbkginfline2qr invdescriptionqr2 swicotaxbaseqr swicotaxqr);
 	push @a, "$form->{vc}number", "$form->{vc}phone", "$form->{vc}fax",
 	  "$form->{vc}taxnumber";
 	push @a, 'email' if !$form->{media} eq 'email';
